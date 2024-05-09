8bb1582d6e34b84349a5fc9582a883be4ae98f52
FoxTail <git@rdstudios.org>
2024-05-09 16:45:28 -0700

    Using BIOS now, interrupts enabled, framebuffer overhaul, println! macro implemented

--- kernel/src/framebuffer.rs
+++ kernel/src/framebuffer.rs
@@ -1,4 +1,11 @@
+use core::{fmt, slice};
+
+use spin::Mutex;
 use bootloader_api::info::{FrameBuffer, FrameBufferInfo};
+
+lazy_static! {
+    pub static ref WRITER: Mutex<FramebufferWriter> = Mutex::new(FramebufferWriter::new());
+}
 
 pub struct Framebuffer {
     // Dont need the actual FrameBuffer obj cuz this was all that was in it anyway
@@ -7,6 +14,7 @@ pub struct Framebuffer {
     color_locations: ColorLocations,
 }
 
+#[allow(dead_code)]
 impl Framebuffer {
     pub fn new(fb: &'static mut FrameBuffer) -> Self {
         let fbi = fb.info();
@@ -15,6 +23,24 @@ impl Framebuffer {
 
         Self {
             fbi, buffer, color_locations
+        }
+    }
+
+    // Just the settings for my pc, but they'll be overwritten so it doesn't really matter
+    fn default() -> Self {
+        let fbi = FrameBufferInfo {
+            byte_len: 3145728,
+            width: 1024,
+            height: 780,
+            pixel_format: bootloader_api::info::PixelFormat::Bgr,
+            bytes_per_pixel: 4,
+            stride: 1024,
+        };
+
+        Self {
+            fbi,
+            buffer: unsafe { slice::from_raw_parts_mut(0x80000000 as *mut u8, fbi.byte_len) },
+            color_locations: ColorLocations::new(&fbi),
         }
     }
 
@@ -58,6 +84,35 @@ impl Framebuffer {
             }
         }
     }
+
+    pub fn write_pixel_byte_opacive(&mut self, x: usize, y: usize, byte: u16, opacity: u16) {
+        if opacity == 0 { return }
+
+        let i = y * self.fbi.stride * self.fbi.bytes_per_pixel + x * self.fbi.bytes_per_pixel;
+
+        let r = self.buffer[i + self.color_locations.r] as u16;
+        let g = self.buffer[i + self.color_locations.g] as u16;
+        let b = self.buffer[i + self.color_locations.b] as u16;
+
+        self.buffer[i + self.color_locations.r] = ((byte * opacity + (255 - opacity) * r) / 255) as u8;
+        self.buffer[i + self.color_locations.g] = ((byte * opacity + (255 - opacity) * g) / 255) as u8;
+        self.buffer[i + self.color_locations.b] = ((byte / 2 * opacity + (255 - opacity) * b) / 255) as u8;
+    }
+
+    pub fn clone(&mut self) -> Self {
+        let c = &self.color_locations;
+        let r = c.r;
+        let g = c.g;
+        let b = c.b;
+
+        let buffer = unsafe { core::slice::from_raw_parts_mut(self.buffer.as_ptr() as *mut u8, self.fbi.byte_len) };
+
+        Self {
+            fbi: FrameBufferInfo { ..self.fbi },
+            buffer,
+            color_locations: ColorLocations { r, g, b }
+        }
+    }
 }
 
 struct ColorLocations {
@@ -78,4 +133,137 @@ impl ColorLocations {
             _ => Self { r: 2, g: 1, b: 0 }
         }
     }
+}
+
+use lazy_static::lazy_static;
+use noto_sans_mono_bitmap::{get_raster, get_raster_width, FontWeight, RasterHeight, RasterizedChar};
+use x86_64::instructions::interrupts;
+
+const LINE_SPACING: usize = 2;
+const LETTER_SPACING: usize = 0;
+
+const BORDER_PADDING: usize = 1;
+
+mod font_constants {
+    use super::*;
+
+    pub const FONT_WEIGHT: FontWeight = FontWeight::Regular;
+    pub const CHAR_RASTER_HEIGHT: RasterHeight = RasterHeight::Size16;
+    pub const CHAR_RASTER_WIDTH: usize = get_raster_width(FONT_WEIGHT, CHAR_RASTER_HEIGHT);
+    pub const BACKUP_CHAR: char = '�';
+}
+
+fn get_char_raster(c: char) -> RasterizedChar {
+    fn get(c: char) -> Option<RasterizedChar> {
+        get_raster(c, font_constants::FONT_WEIGHT, font_constants::CHAR_RASTER_HEIGHT)
+    }
+    get(c).unwrap_or_else(|| get(font_constants::BACKUP_CHAR).expect("BROO EXPLODED. get the raster of the backup char"))
+}
+
+pub struct FramebufferWriter {
+    x: usize,
+    y: usize,
+    fb: Framebuffer,
+}
+
+#[allow(dead_code)]
+impl FramebufferWriter {
+    pub fn new() -> Self {
+        Self {
+            x: BORDER_PADDING,
+            y: BORDER_PADDING,
+            fb: Framebuffer::default(),
+        }
+    }
+
+    pub fn init(&mut self, fb: Framebuffer) {
+        self.fb = fb;
+    }
+
+    pub fn clear(&mut self, clear_color: u64) {
+        self.fb.clear(clear_color);
+    }
+
+    pub fn cpy_buff(&mut self, buffer: &'static [u8], buffer_size: (usize, usize), start: (usize, usize)) {
+        self.fb.cpy_buff(buffer, buffer_size, start);
+    }
+
+    pub fn write_pixel_byte_opacive(&mut self, x: usize, y: usize, byte: u8, opacity: u8) {
+        self.fb.write_pixel_byte_opacive(x, y, byte as u16, opacity as u16);
+    }
+
+    fn newline(&mut self) {
+        self.y += font_constants::CHAR_RASTER_HEIGHT.val() + LINE_SPACING;
+        self.carriage_return();
+    }
+
+    fn carriage_return(&mut self) {
+        self.x = BORDER_PADDING;
+    }
+
+    fn write_char(&mut self, c: char) {
+        match c {
+            '\n' => self.newline(),
+            '\r' => self.carriage_return(),
+            c => {
+                let x = self.x + font_constants::CHAR_RASTER_WIDTH;
+
+                if x > self.fb.fbi.width {
+                    self.newline();
+                }
+
+                let y = self.y + font_constants::CHAR_RASTER_HEIGHT.val() + BORDER_PADDING;
+
+                if y > self.fb.fbi.height {
+                    self.fb.clear(0);
+                }
+
+                self.write_rendered_char(get_char_raster(c));
+            }
+        }
+    }
+
+    fn write_rendered_char(&mut self, c: RasterizedChar) {
+        for (y, row) in c.raster().iter().enumerate() {
+            for (x, byte) in row.iter().enumerate() {
+                self.fb.write_pixel_byte_opacive(self.x + x, self.y + y, *byte as u16, *byte as u16);
+            }
+        }
+
+        self.x += c.width() + LETTER_SPACING;
+    }
+}
+
+impl fmt::Write for FramebufferWriter {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            self.write_char(c);
+        }
+        Ok(())
+    }
+
+    // fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> fmt::Result {
+    //     let _ = self.write_str(args.as_str().unwrap());
+    //     Ok(())
+    // }
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg: tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg: tt)*) => ($crate::framebuffer::_print(format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use fmt::Write;
+
+    interrupts::without_interrupts(|| {
+        WRITER.lock().write_fmt(args).unwrap();
+    });
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,41 +6,46 @@ bootloader_api::entry_point!(kernel_main);
 
 mod framebuffer;
 mod interrupts;
+mod gdt;
 
-use crate::framebuffer::Framebuffer;
+use crate::{framebuffer::{Framebuffer, WRITER}, interrupts::PICS};
 
 #[no_mangle]
+#[allow(unused, non_upper_case_globals)]
 fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
-    let fb: &mut bootloader_api::info::FrameBuffer;
-
-    match &mut boot_info.framebuffer {
-        bootloader_api::info::Optional::None => loop {},
-        bootloader_api::info::Optional::Some(t) => fb = t
+    let fb = match &mut boot_info.framebuffer {
+        bootloader_api::info::Optional::None => panic!(),
+        bootloader_api::info::Optional::Some(t) => t
     };
 
+    gdt::init();
     interrupts::init_idt();
+    unsafe { PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 
     let mut fb2 = Framebuffer::new(fb);
-    fb2.clear(0xb83c3c);
+    x86_64::instructions::interrupts::without_interrupts(|| {
+        let mut w = WRITER.lock();
 
-    const size: usize = 50;
-    const buf_size: usize = size * size * 3;
-    let buf: &[u8; buf_size] = &[255; buf_size];
-    fb2.cpy_buff(buf, (size, size), (100, 100));
+        w.init(fb2);
+        w.clear(0xb83c3c);
+    });
 
-    unsafe {
-        // *(0xdeadbeef as *mut u8) = 42;
-    }
+    println!("TESTING");
 
     loop {
         x86_64::instructions::hlt();
     }
 }
 
+#[allow(unused_imports)]
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop {
         x86_64::instructions::hlt();
     }
