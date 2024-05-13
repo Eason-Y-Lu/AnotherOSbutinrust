f06b13c9c965703d62372589d9cd595bb65ea832
Eason Lu <aelnosu@gmail.com>
2024-05-13 11:31:22 -0700

    Update shell script
    Uodated linuxDocker.sh to attach the container.

--- linuxDocker.sh
+++ linuxDocker.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 xauth list >xauth.txt
 docker build -t anotheros .
-docker run -d --rm --name another_os --net=host --volume /tmp/.X11-unix:/tmp/.X11-unix --env DISPLAY=$DISPLAY anotheros sleep 300
+docker run -dt --rm --name another_os --net=host --volume /tmp/.X11-unix:/tmp/.X11-unix --env DISPLAY=$DISPLAY anotheros
 docker cp xauth.txt another_os:xauth.txt
 docker cp bash_script/stage2.sh another_os:stage2.sh
 shred -u xauth.txt
